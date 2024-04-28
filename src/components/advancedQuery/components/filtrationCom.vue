6ec697d8a4f57b98d560dfb336f04dd6e68ee1c7
蚂蚁呀嘿 <44965324+wsd0727@users.noreply.github.com>
2024-04-28 17:21:38 +0800

    高级查询部分样式

---
+++ src/components/advancedQuery/components/filtrationCom.vue
@@ -0,0 +1,37 @@
+<!--
+ * 快捷过滤组件
+ * @author: wsd
+ * @since: 2024-04-28
+ * filtrationCom.vue
+-->
+<template>
+    <div class="filtrationCom">
+        <el-row :gutter="10" >
+            <el-col :sm="12" :xl="8" v-for="(item, index) in filterArr" :key="index">
+                <FilterForm :filterConfig="filterConfig" />
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+import FilterForm from "./filterForm"
+
+
+const props = defineProps({
+    filterConfig: {
+        type: Object,
+        default: {},
+    },
+    filterArr: {
+        type: Array,
+        default: [],
+    }
+});
+
+console.log(props.filterConfig)
+
+
+</script>
+
+<style scoped lang="scss"></style>
