6400dbf1645ace81604d83cded2077b248fa9589
piplns <piplns@163.com>
2024-05-17 17:59:15 +0800

    del

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,16 @@
 <!--
  * @Author: cc2049
  * @Date: 2024-04-19 09:01:33
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-04-28 17:57:28
+ * @LastEditors: piplns piplns@163.com
+ * @LastEditTime: 2024-05-17 17:58:35
  * @Description: 简介
 -->
 <template>
   <div>
     首页看板
-    <AdvanceQuery />
   </div>
 </template>
 
 <script setup>
-
-import AdvanceQuery from "@/components/AdvancedQuery/index"
 const message = ref('Hello Vue TMS-V3!')
 </script>
