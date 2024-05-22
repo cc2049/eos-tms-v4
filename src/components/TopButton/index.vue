f8159b5a052f2c4d6a575b415b76daecea6102ec
蚂蚁呀嘿 <44965324+wsd0727@users.noreply.github.com>
2024-05-22 11:33:18 +0800

    删除用户无用的

--- src/components/TopButton/index.vue
+++ src/components/TopButton/index.vue
@@ -92,36 +92,6 @@
       </template>
     </vxe-modal>
 
-    <!-- 用户授权 -->
-    <vxe-modal destroy-on-close v-model="pageConfig.modalVisible" :width="pageConfig.modalW" :height="pageConfig.modalH" id="formModal" resize storage transfer show-zoom show-footer>
-      <template #title>
-        <span> {{ pageConfig.modelTitle }} </span>
-      </template>
-      <template #default>
-        <el-row :gutter="10">
-          <!-- <Form ref="formRef" v-model:formData="form" labelWidth="100px" :formConfig="formConfig" :rules="rules" >
-            <template #MENULIST>
-              <div class="menuSelect">
-                <el-checkbox v-model="menuExpand" @change="handleCheckedTreeExpand($event)">展开/折叠</el-checkbox>
-                <el-checkbox v-model="menuNodeAll" @change="handleCheckedTreeNodeAll($event)">全选/全不选</el-checkbox>
-                <el-checkbox v-model="menuFandZ">父子联动</el-checkbox>
-                <el-tree class="tree-border" :data="AllMenuTree" show-checkbox ref="menuRef" node-key="VALUE" :check-strictly="!menuFandZ" empty-text="暂无数据" :props="TreeProps"></el-tree>
-              </div>
-            </template>
-            <template #PANELID>
-                <div>
-                  <el-select v-model.trim="form.PANELID" placeholder="请选择" clearable @change="changePANELID" style="width: 100%;">
-                    <el-option v-for="item in PANELIDList" :key="item.BILLNO" :label="item.PANEL_NAME" :value="item.BILLNO"></el-option>
-                  </el-select>
-                </div>
-              </template>
-          </Form> -->
-        </el-row>
-      </template>
-      <template #footer>
-        <el-button type="primary" size="default" @click="formSubmit">保存</el-button>
-      </template>
-    </vxe-modal>
 
   </div>
 
@@ -135,9 +105,6 @@ import { getUrlParams } from "@/utils";
 import { getFormValue, getFormRule } from "@/utils";
 
 import FormPage from "@/views/formPage/index.vue";
-
-import { getMENUBENTree, RoleDetail } from "#/system/role";
-import { getAuthRoles } from "#/system/user";
 
 const route = useRoute();
 const topButton = defineModel("topButton");
@@ -153,59 +120,9 @@ const props = defineProps({
   },
 });
 
-// 用户授权===============================================================
-const data = reactive({
-  baseForm: {},
-  form: {},
-  formConfig: [],
-  AllMenuTree: [],
-  queryParams: {
-    PAGENUM: 1,
-    PAGESIZE: 10,
-    MODULEID: route.meta.BILLNO,
-    PAGEID: route.meta.PBILLNO,
-    PK_ORG: undefined,
-    PK_DEPT: undefined,
-  },
-  BILLSTATUS: [
-    { LABEL: "正常", VALUE: "1", COLOR: "primary" },
-    { LABEL: "停用", VALUE: "0", COLOR: "danger" },
-    { LABEL: "锁定", VALUE: "2", COLOR: "danger" },
-  ],
-  rules: {},
-  TreeProps: {
-    label: "LABEL",
-    children: "CHILDREN",
-    disabled: (data) => {
-      let { VALUE } = data;
-      return roleData.value.includes(VALUE);
-    },
-  },
-});
-const roleData = ref([]);
-const {
-  queryParams,
-  baseForm,
-  form,
-  formConfig,
-  BILLSTATUS,
-  AllMenuTree,
-  rules,
-  TreeProps,
-} = toRefs(data);
-// 菜单权限相关
-const menuRef = ref();
-const menuExpand = ref(false);
-const menuNodeAll = ref(false);
-const menuFandZ = ref(true);
 
-// 弹窗
-const pageConfig = reactive({
-  modalVisible: false,
-  modalW: "50%",
-  modalH: "50%",
-  modelTitle: "",
-});
+
+
 
 const isGetDetail = ref(false);
 const activeBtn = ref(null);
@@ -213,84 +130,7 @@ function closeModal() {
   modalConfig.modalVisible = false;
 }
 
-// 初始化 用户授权 表单store
-const initPERMISSForm = () => {
-  let config = [
-    {
-      FIELD: "VNAME",
-      LABEL: "用户",
-      COL: 24,
-      CONTROLS: "ExReadCard",
-      ISREQUIRE: 1,
-      ISSHOW: 1,
-      OTHER: "",
-    },
-    {
-      FIELD: "ROLENAME",
-      LABEL: "角色",
-      COL: 24,
-      CONTROLS: "ExReadCard",
-      ISREQUIRE: 1,
-      ISSHOW: 1,
-      OTHER: "",
-    },
-    {
-      FIELD: "MENULIST",
-      LABEL: "菜单权限",
-      COL: 24,
-      CONTROLS: "slot",
-      ISSHOW: 1,
-    },
-  ];
-  formConfig.value = config;
-  let formData = getFormValue(config);
-  baseForm.value = formData;
-  form.value = formData;
-  rules.value = getFormRule(config);
-  pageConfig.modalW = "600px";
-  pageConfig.modalH = "70%";
-};
-/** 树权限（展开/折叠）*/
-function handleCheckedTreeExpand(value) {
-  let treeList = AllMenuTree.value;
-  // for (let i = 0; i < treeList.length; i++) {
-  //   menuRef.value.store.nodesMap[treeList[i].VALUE].expanded = value;
-  // }
-}
 
-const handlePermiss = (row) => {
-  // formType.value = "permiss";
-  let { BILLNO, PK_ROLE } = row;
-
-  getMENUBENTree({
-    BILLFROM: "0",
-  }).then((res) => {
-    AllMenuTree.value = res.RESULT;
-    initPERMISSForm();
-    form.value = row;
-    RoleDetail({
-      BILLNO: PK_ROLE,
-    }).then((res) => {
-      roleData.value = [...res.RESULT.MENU, ...res.RESULT.BTN];
-      console.log(BILLNO);
-      getAuthRoles({ BILLNO }).then((res) => {
-        pageConfig.modelTitle = "用户授权";
-        let selectMenu = [...roleData.value];
-        pageConfig.modalVisible = true;
-        if (res.RESULT) {
-          selectMenu = [...selectMenu, ...res.RESULT.MENU, ...res.RESULT.BTN];
-        }
-        nextTick(() => {
-          menuExpand.value = true;
-          handleCheckedTreeExpand(true);
-          // selectMenu.forEach((v) => {
-          //   menuRef.value.setChecked(v, true, false);
-          // });
-        });
-      });
-    });
-  });
-};
 
 const MenuID = inject("menuID");
 const formID = ref(null);
@@ -476,26 +316,7 @@ function handleEvent(data) {
     downFilesByUrl(data);
   } else if (data.VTYPE == 21) {
     console.log(selectRecords);
-    switch (data.BTNTITLE) {
-      case "userAuthorization": // 用户授权
-        handlePermiss(selectRecords[0]);
-        break;
-      case "userOrgScope": // 用户组织授权
-        // emit('update', 'hour', cycleTotal.value, 'hour')
-        break;
-      // case 3:
-      //     emit('update', 'hour', averageTotal.value, 'hour')
-      //     break
-      // case 4:
-      //     if (checkboxList.value.length === 0) {
-      //         checkboxList.value.push(checkCopy.value[0])
-      //     } else {
-      //         checkCopy.value = checkboxList.value
-      //     }
-      //     emit('update', 'hour', checkboxString.value, 'hour')
-      //     break
-    }
-    //
+    
   }
 }
 
